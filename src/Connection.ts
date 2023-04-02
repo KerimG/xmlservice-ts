75cbc00f1defe7c9ea8e2ce08749a0f7079317b9
Kerim Güney <kerim@gueney.io>
2023-04-02 13:37:45 +0200

    Implement starting calling stateful XMLSERVICE-CLI

--- src/Connection.ts
+++ src/Connection.ts
@@ -1,41 +1,96 @@
 import { spawn } from 'child_process';
+import { randomUUID } from 'crypto';
 
-interface ConnectionConfig {
+export interface ConnectionConfig {
   xmlservicePath?: string;
   stateful?: boolean;
+  ipcPath?: string;
+}
+
+export interface XmlserviceResult {
+  output: string | null;
+  signal: string | null;
+  code: number | null;
 }
 
 export class Connection {
   #xmlservicePath: string;
-  stateful: boolean;
+  #stateful: boolean;
+  #ipcPath: string;
+
+  #xmlserviceParams: string[];
 
   constructor(config: ConnectionConfig) {
-    this.stateful = config.stateful || false;
     this.#xmlservicePath = config.xmlservicePath || '/QOpenSys/pkgs/bin/xmlservice-cli';
+    this.#ipcPath = '';
+    this.#stateful = config.stateful || false;
+    this.#xmlserviceParams = [];
+
+    if (this.#stateful) {
+      this.#ipcPath = config.ipcPath || `/tmp/xmlservice-${randomUUID()}`;
+      this.#xmlserviceParams.push('-c', '*sbmjob', '-i', this.#ipcPath);
+    }
   }
 
-  execute(xmlIn: string) {
+  execute(xmlIn?: string): Promise<XmlserviceResult> {
     return new Promise((resolve, reject) => {
-      console.log(this.#xmlservicePath);
-      const xmlservice = spawn(this.#xmlservicePath);
-      let data: Buffer[] = [];
+      const inputBuffer = xmlIn ? Buffer.from(xmlIn) : null;
+      const outputBuffer: Buffer[] = [];
+      const result: XmlserviceResult = {
+        output: null,
+        signal: null,
+        code: null,
+      };
+
+      const xmlservice = spawn(this.#xmlservicePath, this.#xmlserviceParams);
 
       xmlservice.stdout.on('data', (chunk) => {
-        data.push(chunk);
+        outputBuffer.push(chunk);
       });
 
       xmlservice.on('close', (code, signal) => {
-        const output = Buffer.concat(data).toString();
+        result.output = Buffer.concat(outputBuffer).toString();
+        result.code = code;
+        result.signal = signal;
 
         if (code === 0) {
-          resolve(output);
+          resolve(result);
         } else {
-          reject(output);
+          reject(result);
         }
       });
 
-      xmlservice.stdin.write(`${xmlIn}\n`);
-      xmlservice.stdin.end();
+      if (xmlIn) {
+        xmlservice.stdin.write(inputBuffer);
+        xmlservice.stdin.end();
+      }
+    });
+  }
+
+  end(): Promise<XmlserviceResult> {
+    return new Promise((resolve, reject) => {
+      const result: XmlserviceResult = {
+        output: null,
+        signal: null,
+        code: null,
+      };
+      if (this.#stateful) {
+        const xmlservice = spawn(this.#xmlservicePath, ['-c', '*immed', '-i', this.#ipcPath]);
+
+        xmlservice.on('close', (code, signal) => {
+          result.code = code;
+          result.signal = signal;
+
+          if (code === 0) {
+            resolve(result);
+          } else {
+            reject(result);
+          }
+        });
+      } else {
+        result.code = 0;
+        resolve(result);
+      }
     });
   }
 }
